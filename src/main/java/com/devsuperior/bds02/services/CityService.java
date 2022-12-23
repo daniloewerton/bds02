f1014023711ca1a9ba6fd2d034020f6d593b6298
Danilo Ewerton <danilo.ewe@gmail.com>
2022-12-23 18:42:11 -0300

    CityService implementado

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,2 +1,45 @@
-package com.devsuperior.bds02.services;public class CityService {
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exception.DatabaseException;
+import com.devsuperior.bds02.services.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll() {
+
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+
+    public CityDTO insert(final CityDTO dto) {
+        City city = new City();
+        city.setId(dto.getId());
+        city.setName(dto.getName());
+        return new CityDTO(repository.save(city));
+    }
+
+    public void delete(final Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado.");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Operação não permitida.");
+        }
+    }
 }
