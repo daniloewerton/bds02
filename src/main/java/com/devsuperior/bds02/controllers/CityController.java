91243ff9beb4bf14180487fbd6ea05798f2f14a1
Danilo Ewerton <danilo.ewe@gmail.com>
2022-12-23 18:41:37 -0300

    Exceptions implementadas

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.bds02;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody final CityDTO dto) {
+        CityDTO entity = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/cities").build().toUri();
+        return ResponseEntity.created(uri).body(entity);
+    }
+}
